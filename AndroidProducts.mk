77213d242caa40c7759645293e3c0c6dabaf6c3e
Louis Popi <theh2o64@gmail.com>
2017-04-11 06:33:16 +0200

    lettuce: Adapt for CYOS build system
    Change-Id: Ib4311fce520c976ed7a12164024b99486882c158

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from lettuce device
-$(call inherit-product, device/yu/lettuce/device.mk)
-
-# Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := lettuce
-PRODUCT_NAME := full_lettuce
-PRODUCT_BRAND := YU
-PRODUCT_MODEL := YU5010
-PRODUCT_MANUFACTURER := YU
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/cyos_lettuce.mk
--- cyos_lettuce.mk
+++ cyos_lettuce.mk
@@ -1,28 +1,36 @@
+#
 # Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-$(call inherit-product, device/yu/lettuce/full_lettuce.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from lettuce device
+$(call inherit-product, device/yu/lettuce/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := lettuce
+PRODUCT_NAME := cyos_lettuce
+PRODUCT_BRAND := YU
+BOARD_VENDOR := yu
+PRODUCT_MODEL := YU5010
+PRODUCT_MANUFACTURER := YU
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
-
-PRODUCT_NAME := lineage_lettuce
-BOARD_VENDOR := yu
-PRODUCT_DEVICE := lettuce
 
 PRODUCT_GMS_CLIENTID_BASE := android-micromax
 
@@ -36,3 +44,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=YU/YUPHORIA/YUPHORIA:5.1.1/LMY49J/YOG4PAS8A8:user/release-keys \
     PRIVATE_BUILD_DESC="YUPHORIA-user 5.1.1 LMY49J YOG4PAS8A8 release-keys"
 endif
+
